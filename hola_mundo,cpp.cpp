4b260b5a0ae2357cd49bf620e52f0b54481afc70
c. atencio <carlosshooter2909@gmail.com>
2026-01-29 12:55:57 -0400

    feat: agregar mas mensajes al programa

--- hola_mundo,cpp.cpp
+++ hola_mundo,cpp.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main() {
     cout << "¡Hola Mundo desde Git!" << endl;
     cout << "Mi nombre es: c. atencio" << endl;
+    cout << "Estoy aprendiendo Git y Github." << endl;
+    cout << "Este es mi segundo commit" << endl;
     return 0;
 }
